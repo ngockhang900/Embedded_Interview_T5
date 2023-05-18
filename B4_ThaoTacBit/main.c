0489fdd17511d1633458aa678f10bbf19c877333
Đỗ Ngọc Khang <dongockhang2@gmail.com>
2023-05-18 22:04:00 +0700

    update B4_ThaoTaBit file

--- B4_ThaoTacBit/main.c
+++ B4_ThaoTacBit/main.c
@@ -12,40 +12,76 @@ typedef enum{
     pin7,
 }pins;
 
+typedef enum{
+    High,
+    Low,
+}s_status;
+
 uint8_t porta = 0b00000000;
 uint8_t portb = 0b11111111;
 
 void pinHigh(pins pin){
     porta |=  (0b10000000 >> pin);
-}
-
-void pinLow(pins pin){
-    portb &=  ~(10000000 >> pin);
-}
-
-void readBit(uint8_t byte){
-    uint8_t temp = 0b10000000;
     uint8_t kq;
     printf("0b");
     for (uint8_t i = 0; i < 8; i++)
     {
-        kq = byte & temp;
+        kq = porta & 0b10000000;
         if(kq == 0){
             printf("0");
         }else{
             printf("1");
         }
-        byte = byte << 1;
+        porta = porta << 1;
     }
     printf("\n");
-    
 }
+
+void pinLow(pins pin){
+    portb &=  ~(0b10000000 >> pin);
+    uint8_t kq;
+    printf("0b");
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        kq = portb & 0b10000000;
+        if(kq == 0){
+            printf("0");
+        }else{
+            printf("1");
+        }
+        portb = portb << 1;
+    }
+    printf("\n");
+}
+
+// void readBit(uint8_t byte){
+//     uint8_t kq;
+//     printf("0b");
+//     for (uint8_t i = 0; i < 8; i++)
+//     {
+//         kq = byte & 0b10000000;
+//         if(kq == 0){
+//             printf("0");
+//         }else{
+//             printf("1");
+//         }
+//         byte = byte << 1;
+//     }
+//     printf("\n");
+// }
+
+void digitalWrite(pins pin, s_status status){
+    if(status == High){
+        pinHigh(pin);
+    }else{
+        pinLow(pin);
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
-    pinHigh(pin3);
-    readBit(porta);
-    pinLow(pin3);
-    readBit(portb);
+    digitalWrite(pin6, High);
+    digitalWrite(pin6, Low);
     
 }
